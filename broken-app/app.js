ee486f6ceccca8432f479e8eeb5eccdb952f977c
dave-home-2 <davemcelhaney@gmail.com>
2023-02-19 17:32:39 -0500

    may have fixed, waiting for api limits

--- broken-app/app.js
+++ broken-app/app.js
@@ -2,14 +2,20 @@ const express = require('express');
 let axios = require('axios');
 var app = express();
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+app.use(express.json());
 
-    return res.send(JSON.stringify(out));
+app.post('/', async function(req, res, next) {
+  try {
+    let results = results.map(async d => {
+      await axios.get(`https://api.github.com/users/${d}`)
+    });
+    Promise.all(results)
+    .then(() => {
+      let out = sortedResults.map( r => ({
+          name: r.data.name, bio: r.data.bio }));
+      return res.send(JSON.stringify(out));
+    })
+
   } catch(err) {
     next(err);
   }
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,3 @@
 # Broken App Issues
+
+need to utilized promise.all to await all results before trying to submit our first get request to axios.
